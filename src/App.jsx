7e5dc07b26db2a60966d1f000f14ef4c9204d631
Pruthviraj Talap <pruthvirajtalap2001@gmail.com>
2026-01-03 11:08:46 +0530

    changed router code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -15,7 +15,6 @@ function App() {
       <CustomCursor />
       <div className="relative z-0 bg-primary w-full min-h-screen">
         <Navbar />
-        <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -24,7 +23,6 @@ function App() {
           <Route path="/skills" element={<Skills />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        </HashRouter>
       </div>
     </Router>
   );
